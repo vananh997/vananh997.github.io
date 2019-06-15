ac62f143056a03f675f7e0ffc6eba2e47be31c88
vanhh <vananh997@github.com>
2019-06-15 19:28:18 +0700

    Update function.js

--- BT13_0606_Calculator/js/function.js
+++ BT13_0606_Calculator/js/function.js
@@ -1,39 +1,35 @@
+let x = document.getElementById('result');
+
 function del(){
 	document.getElementById('result').value = "";
 }
 
-function back(){
-	var x = result.value;
+function back(){	
 	document.getElementById('result').value = x.substring(0, x.length - 1);
 }
 
 function sqrt(){
-	var x = result.value;
 	document.getElementById('result').value = Math.sqrt(x) ;
 }
 
-function tan(){
-	var x = result.value;
+function tan(){	
 	document.getElementById('result').value = Math.tan(x) ;
 }
 
 function increase(){
-	var x = result.value;
 	document.getElementById('result').value = x*x;
 }
 
 function sin(){
-	var x = result.value;
 	document.getElementById('result').value = Math.sin(x * Math.PI / 180);
 }
 
 function cos(){
-	var x = result.value;
 	document.getElementById('result').value = Math.cos(x * Math.PI / 180);
 }
 
 function PI(){
-	var x = result.value;
 	document.getElementById('result').value = Math.PI;
 }
+
 
