29f9eee346c11de2c3084ad76b116b988c572c4d
vananh997 <vananh997@github.io>
2019-06-29 19:54:14 +0700

    BT15_1306_Register Form

--- BT15_1306_Register Form/js/profile.js
+++ BT15_1306_Register Form/js/profile.js
@@ -1,14 +1,49 @@
-function profile() {
-    var ho_ten = document.getElementById('ho_ten').value;
-    var email = document.getElementById('email').value;
-    var mat_khau = document.getElementById('mat_khau').value;
-    var ngay_sinh = document.getElementById('ngay_sinh').value;
-    var sdt = document.getElementById('sdt').value;
-    var dia_chi = document.getElementById('dia_chi').value;
+$(document).ready(function() {
+    $(window).on('load', function LayThongTinDangKy() {
+        let uri = document.URL;
+        let urinew = document.URL.split('?')[0];
+        let uri_dec = decodeURIComponent(uri);
+        let content = uri_dec.replace(/[&?]/g, ' ').replace(/[+]/g, ' ').replace(/[=]/g, ': ');
+        let total = content.replace(urinew, '').trim().split(' ');
+        let hoTen = '';
+        let ngaySinh = '';
+        let gioiTinh = '';
+        let diaChi = '';
+        let soDT = '';
+        let email = '';
+        let pass = '';
+        for (let i = 1; i < total.length; i++) {
+            if (i < total.indexOf('birthday_day:', 1)) {
+                hoTen += total[i] + ' ';
+                $('#ho_ten').text(hoTen);
 
-    document.getElementById('hoTen').innerText = ho_ten;
-    document.getElementById('email').innerText = email;
-    document.getElementById('matKhau').innerText = mat_khau;
-    document.getElementById('ngaySinh').innerText = ngay_sinh;
-    document.getElementById('sdt').innerText = sdt;
-}
+            }
+            if (i > total.indexOf('birthday_day:', 1) && i < total.indexOf('gender:', 1) &&
+                total[i] != 'birthday_month:' && total[i] != 'birthday_year:') {
+                ngaySinh += total[i] + '/';
+                $('#ngay_sinh').text(ngaySinh.substring(0, ngaySinh.length - 1));
+            }
+            if (i > total.indexOf('gender:', 1) && i < total.indexOf('diachi:', 1)) {
+                gioiTinh += total[i];
+                $('#gioi_tinh').text(gioiTinh);
+            }
+            if (i > total.indexOf('diachi:', 1) && i < total.indexOf('sodt:', 1)) {
+                diaChi += total[i] + ' ';
+                $('#dia_chi').text(diaChi);
+            }
+            if (i > total.indexOf('sodt:', 1) && i < total.indexOf('email:', 1)) {
+                soDT += total[i];
+                $('#sdt').text(soDT);
+            }
+            if (i > total.indexOf('email:', 1) && i < total.indexOf('matkhau:', 1)) {
+                email += total[i];
+                $('#email').text(email);
+            }
+            if (i > total.indexOf('matkhau:', 1)) {
+                pass += total[i];
+                $('#matKhau').text(pass)
+            }
+
+        }
+    });
+});
